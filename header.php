5eaa8e6b6bcf16a873c8cb2adb05b1cf288b1e46
HayanoRyuki <hayano@media-confidence.com>
2025-12-17 10:47:36 +0900

    fix: remove hardcoded favicon from header.php

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="shortcut icon" href="#" type="image/x-icon" />
 
   <!-- Google Tag Manager -->
   <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
