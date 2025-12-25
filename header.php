01ee24bc8bd48a50de0f88999160687a4f4ce2d9
HayanoRyuki <hayano@media-confidence.com>
2025-12-25 11:22:01 +0900

    fix: load front-page.css directly from header.php

--- header.php
+++ header.php
@@ -42,14 +42,19 @@
     rel="stylesheet">
 
   <!-- favicon -->
-<link rel="icon" type="image/png" sizes="32x32"
-      href="<?php echo get_template_directory_uri(); ?>/assets/img/icon_3c.png">
-<link rel="icon" type="image/png" sizes="16x16"
-      href="<?php echo get_template_directory_uri(); ?>/assets/img/icon_3c.png">
-<link rel="apple-touch-icon"
-      href="<?php echo get_template_directory_uri(); ?>/assets/img/icon_3c.png">
-<link rel="icon" sizes="192x192"
-      href="<?php echo get_template_directory_uri(); ?>/assets/img/icon_3c.png">
+  <link rel="icon" type="image/png" sizes="32x32"
+        href="<?php echo get_template_directory_uri(); ?>/assets/img/icon_3c.png">
+  <link rel="icon" type="image/png" sizes="16x16"
+        href="<?php echo get_template_directory_uri(); ?>/assets/img/icon_3c.png">
+  <link rel="apple-touch-icon"
+        href="<?php echo get_template_directory_uri(); ?>/assets/img/icon_3c.png">
+  <link rel="icon" sizes="192x192"
+        href="<?php echo get_template_directory_uri(); ?>/assets/img/icon_3c.png">
+
+  <!-- front-page main CSS -->
+  <link rel="stylesheet"
+        href="<?php echo get_template_directory_uri(); ?>/assets/css/front-page.css">
+
   <?php wp_head(); ?>
 </head>
 
